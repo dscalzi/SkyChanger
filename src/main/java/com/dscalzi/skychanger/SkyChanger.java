f2a70fd1805bf07477398850cb57ca65e1e50a4f
Daniel Scalzi <d_scalzi@yahoo.com>
2017-08-20 18:57:10 -0400

    Updated bStats to 1.2

--- src/main/java/com/dscalzi/skychanger/SkyChanger.java
+++ src/main/java/com/dscalzi/skychanger/SkyChanger.java
@@ -5,7 +5,7 @@
  */
 package com.dscalzi.skychanger;
 
-import org.bstats.Metrics;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 import com.dscalzi.skychanger.managers.ConfigManager;
 import com.dscalzi.skychanger.managers.MessageManager;
@@ -20,12 +20,7 @@ public class SkyChanger extends JavaPlugin{
 		MessageManager.initialize(this);
 		this.getCommand("skychanger").setExecutor(new MainExecutor(this));
 		metrics = new Metrics(this);
-		metrics.addCustomChart(new Metrics.SimplePie("used_language") {
-	        @Override
-	        public String getValue() {
-	            return MessageManager.Languages.getByID(ConfigManager.getInstance().getLanguage()).getReadable();
-	        }
-	    });
+		metrics.addCustomChart(new Metrics.SimplePie("used_language", () -> MessageManager.Languages.getByID(ConfigManager.getInstance().getLanguage()).getReadable()));
 	}
 	
 	@Override
