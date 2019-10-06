286482e8581d4709d5a75281cb642cf22b712b1f
Daniel Scalzi <d_scalzi@yahoo.com>
2019-10-06 03:18:02 -0400

    Update license header.

--- SkyChanger-Bukkit/src/main/java/com/dscalzi/skychanger/bukkit/SkyChangerPlugin.java
+++ SkyChanger-Bukkit/src/main/java/com/dscalzi/skychanger/bukkit/SkyChangerPlugin.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of SkyChanger, licensed under the MIT License (MIT).
  *
- * Copyright (c) 2017-2018 Daniel D. Scalzi <https://github.com/dscalzi/SkyChanger>
+ * Copyright (c) 2017-2019 Daniel D. Scalzi <https://github.com/dscalzi/SkyChanger>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
